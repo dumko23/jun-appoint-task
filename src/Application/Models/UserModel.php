809983fa0e9522c52a93063937f171bc7b0d14ff
Dima Parkhomenko <80668534+dumko23@users.noreply.github.com>
2022-11-08 01:01:25 +0200

    Update UserModel.php
    Password leak fix

--- src/Application/Models/UserModel.php
+++ src/Application/Models/UserModel.php
@@ -79,7 +79,7 @@ class UserModel extends Model
                 $mail->Host = 'smtp.gmail.com';                       //Set the SMTP server to send through
                 $mail->SMTPAuth = true;                                   //Enable SMTP authentication
                 $mail->Username = 'mailsender.parhomenko@gmail.com';      //SMTP username mailsender.parhomenko@gmail.com
-                $mail->Password = 'rxvgcgeqlizepzsj';                         //SMTP password MySenderPass
+                $mail->Password = '';                         //SMTP password 
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port = 465;                                    //TCP port to connect to;
                 // use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
