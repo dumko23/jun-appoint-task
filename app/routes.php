538cbcb6c5c640d046954b6e2180695438ee9aff
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-11-09 11:16:16 +0200

    Removing unnecessary routes

--- app/routes.php
+++ app/routes.php
@@ -2,14 +2,10 @@
 
 declare(strict_types=1);
 
-use App\Application\Actions\User\ListUsersAction;
-use App\Application\Actions\User\ViewUserAction;
-use App\Application\Middleware\SessionsMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
-use Slim\Views\Twig;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -58,10 +54,5 @@ return function (App $app) {
             ->setName('adminLogout');
         $group->post('/deleteUser', '\App\Application\Controllers\AdminController:deleteUser')
             ->setName('deleteUser');
-    });
-
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
     });
 };
